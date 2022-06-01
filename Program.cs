042270cb18bec033b99f62747440bfdab98ac061
Frederick Lopez <mtester010@gmail.com>
2022-06-01 09:07:53 -0500

    Trying out to solve and get all the duplicates on first try

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿//Master Branch
+﻿// Function to get all duplicate numbers and store it to new array
+int[] intArray = new int[] { 1, 2, 3, 4, 2, 1, 4, 5, 2, 1, };
+int[] myDuplicateNum = GetDuplicate(intArray);
+
+int[] GetDuplicate(int[] myArray)
+{
+    int[] duplicates = new int[myArray.Length];
+    int counter = 1;
+
+    for (int index = 0; index < myArray.Length; index++)
+    {
+        if (myArray[index] == myArray[counter])
+        {
+            duplicates[index] = myArray[index];
+            counter++;
+        }
+        else if (myArray[index] == duplicates[index])
+        {
+            duplicates[index] = myArray[index];
+        }
+    }
+
+    return duplicates;
+}
+
+Console.WriteLine(String.Join(' ', myDuplicateNum));
