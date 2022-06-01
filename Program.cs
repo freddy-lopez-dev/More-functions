fc24280248bf973db6cb4b9bfdbd67ad9bfd2055
Frederick Lopez <mtester010@gmail.com>
2022-06-01 10:54:27 -0500

    Add function for getting integers for users

--- Program.cs
+++ Program.cs
@@ -1,66 +1,20 @@
-﻿// Function to get all duplicate numbers and store it to new array
-//int[] intArray = new int[] { 1, 2, 3, 4, 2, 1, 4, 5, 2, 1, };
-//int[] myDuplicateNum = GetDuplicate(intArray);
+﻿int[] testValues = GetIntegersFromUser();
 
-//int[] GetDuplicate(int[] myArray)
-//{
-//    int[] duplicates = new int[myArray.Length];
-//    int counter = 1;
 
-//    for (int index = 0; index < myArray.Length; index++)
-//    {
-//        if (myArray[index] == myArray[counter])
-//        {
-//            duplicates[index] = myArray[index];
-//            counter++;
-//        }
-//        else if (myArray[index] == duplicates[index])
-//        {
-//            duplicates[index] = myArray[index];
-//        }
-//    }
-
-//    return duplicates;
-//}
-
-//Console.WriteLine(String.Join(' ', myDuplicateNum));
-
-// IN CLASS SOl.
-// Start with function first
-// To run all elements in array comparing each
-FindDuplicateNumbers(new int[] { 1,2,2,3,4,3,2,7 });
-
-int[] FindDuplicateNumbers(int[] numbers)
+int[] GetIntegersFromUser()
 {
-    int[] duplicateNumbers = new int[0]; //Empty array assign
+    int[] numbers = new int[0];
 
-    for(int outerIndex = 0; outerIndex < numbers.Length; outerIndex++)
+    Console.WriteLine("Enter a number, or q to stop");
+
+    string inputString = Console.ReadLine();
+
+    while(inputString != "q")
     {
-        for (int innerIndex = outerIndex + 1 ; innerIndex < numbers.Length; innerIndex++)
-        {
-            if(numbers[innerIndex] == numbers[outerIndex])
-            {
-                bool alreadyExist = false;
-
-                for (int dupeIndex = 0; dupeIndex < duplicateNumbers.Length; dupeIndex++)
-                {
-                    if(duplicateNumbers[dupeIndex] == numbers[innerIndex])
-                    {
-                        alreadyExist = true;
-                        dupeIndex = duplicateNumbers.Length;
-                    }
-                }
-                if (!alreadyExist)
-                {
-                    //Console.WriteLine($"{innerIndex} matches {outerIndex}");//String interpolation
-                    Array.Resize(ref duplicateNumbers, duplicateNumbers.Length + 1);
-                    duplicateNumbers[duplicateNumbers.Length - 1] = numbers[innerIndex];
-                    innerIndex = numbers.Length;
-                }
-                
-            }
-        }
+        Array.Resize(ref numbers, numbers.Length + 1);
+        numbers[numbers.Length - 1] = int.Parse(inputString);
+        inputString = Console.ReadLine();
     }
 
-    return duplicateNumbers;
+    return numbers;
 }
