37c3d960f34351f557f7a8e77bb61887707e37cc
Frederick Lopez <mtester010@gmail.com>
2022-05-31 21:08:56 -0500

    Add perfect number functionality

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿//Master Branch
+﻿//Perfect Number Branch
+
+int input = Int32.Parse(Console.ReadLine());
+
+//Fill in the number checks
+
+int SumOfALlNum = GetTheDivisor(input);
+
+int GetTheDivisor(int userNumber)
+{   
+    int total = 0;
+
+    for(int index = 1; index < userNumber; index++)
+    {
+        if (userNumber%index == 0)
+        {
+            total = total + index;
+        }
+    }
+
+    return total;
+}
+
+if (SumOfALlNum == input)
+{
+    Console.WriteLine("Your number is perfect ", input);
+} else
+{
+    Console.WriteLine("Your number is not perfect", input);
+}
